27c83254f43a3cbeaa6f068626fae3a11eac238a
LeafChage <t.kusanagi5512@gmail.com>
2024-04-07 11:46:46 +0900

    add command to open log file

--- vim/nvim/nvim/lua/command.lua
+++ vim/nvim/nvim/lua/command.lua
@@ -1,1 +1,8 @@
 vim.cmd.command('M', 'messages')
+
+vim.api.nvim_create_user_command('OpenLspLog', function()
+        local path = vim.lsp.get_log_path()
+        vim.cmd(":n " .. path)
+    end,
+    { nargs = 0, desc = "open lsp log file" }
+)
