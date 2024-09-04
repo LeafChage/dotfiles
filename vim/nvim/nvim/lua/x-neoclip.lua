7b2bf9dc08e330935d6d5be8b633eeb966c60658
LeafChage <t.kusanagi5512@gmail.com>
2024-09-04 09:17:18 +0900

    change setting of neoclip

--- vim/nvim/nvim/lua/x-neoclip.lua
+++ vim/nvim/nvim/lua/x-neoclip.lua
@@ -1,24 +1,48 @@
 local ok, neoclip = pcall(require, 'neoclip')
 if not ok then return end
 
-neoclip.setup({
-    -- enable_persistent_history = true,
-    on_select = {
-        move_to_front = true,
-        close_telescope = true,
-    },
-    keys = {
-        telescope = {
-            i = {
-                select = '<cr>',
-                paste = '<cr>',
-                replay = '<c-q>',
-                delete = '<c-d>',
-                edit = '<c-e>',
-                custom = {},
-            },
-        },
-    },
-})
+local DBCMD = "sqlite3"
 
-return neoclip
+local checkExistSqlite = function()
+    local handle = io.popen("which " .. DBCMD)
+    if (handle == nil) then
+        return false
+    end
+    ---@type string
+    local result = handle:read('*a')
+    handle:close();
+    return result:match(DBCMD) ~= nil
+end
+
+---@return boolean
+local checkEnablePersistentHistory = function()
+    return checkExistSqlite()
+end
+
+---@return table
+local generateConfig = function()
+    local config = {
+        on_select = {
+            move_to_front = true,
+            close_telescope = true,
+        },
+        keys = {
+            telescope = {
+                i = {
+                    select = '<cr>',
+                    paste = '<cr>',
+                    replay = '<c-q>',
+                    delete = '<c-d>',
+                    edit = '<c-e>',
+                    custom = {},
+                },
+            },
+        }
+    }
+
+    -- save to sqlite
+    config.enable_persistent_history = checkEnablePersistentHistory()
+    return config
+end
+
+return neoclip.setup(generateConfig())
